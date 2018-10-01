b89cf5bbee2a5e160d85328686796ed9f657edd6
Erdem <erdem_kirsehir_40@hotmail.com>
2018-10-01 11:14:32 +0200

    EnergyType Created

---
+++ EnergyType.php
@@ -0,0 +1,28 @@
+<?php
+
+class EnergyType
+{
+	public $EnergyType;
+	
+
+	function __construct ($energytype)
+	{
+		$this->EnergyType = $energytype;
+		
+	
+	} 
+
+	public function EnergyType()
+	{
+		return $this->EnergyType;
+	
+	}
+
+public function __toString()
+	{
+	return json_encode($this);
+	} 
+
+
+	
+}
